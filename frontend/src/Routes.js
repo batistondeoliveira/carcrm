6a73e4d202b7bc635a881010cac202c0a2ff13ba
batistondeoliveira <batistondeoliveira@yahoo.com.br>
2021-04-22 22:17:02 -0300

    Incluido novas rotas do sistema

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -1,13 +1,15 @@
 import React, { Suspense, lazy } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { CircularProgress } from '@material-ui/core';
+import Auth from './view/auth';
 
 const Routes = () => (
     <Router>
         <Suspense fallback={<div className="d-flex justify-content-center mt-5 pt-5"> <CircularProgress /> </div>}>
             <Switch>
-                <Route path="/cadastro" component={() => ( <h1>Cadastro</h1> )} />
-                <Route path="/login" component={() => ( <h1>Login</h1> )} />
+                <Route exact path="/vehicles" component={() => (<h1>Veiculos</h1>)} /> 
+                <Route exact path="/login" component={Auth} /> 
+                <Route exact path="/" component={Auth} />                                
             </Switch>
         </Suspense>
     </Router>
