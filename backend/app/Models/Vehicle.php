d1e3d0d24e8b11e7010817aee264bb809b77f515
batistondeoliveira <batistondeoliveira@yahoo.com.br>
2021-04-24 17:55:09 -0300

    Incluido atributo protegido para corrigir erro de gravacao em massa

--- backend/app/Models/Vehicle.php
+++ backend/app/Models/Vehicle.php
@@ -7,5 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Vehicle extends Model
 {
-    use HasFactory;
+    use HasFactory;    
+
+    protected $guarded = ['id'];
+    
+    public function vehicle_photos() 
+    {
+        return $this->hasMany('App\Models\Vehicle_photos', 'vehicle_id', 'id')->orderBy('order', 'ASC');
+    }
 }
