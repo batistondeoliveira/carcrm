45f7a6278f97bc105dbafc6f8fc53baaf6f816b5
batistondeoliveira <batistondeoliveira@yahoo.com.br>
2021-04-24 20:24:58 -0300

    Incluido a busca do cep no viacep

---
+++ backend/app/Http/Controllers/webservice/WebServiceController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\webservice;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Curl;
+
+class WebServiceController extends Controller
+{
+    public function cep(Request $request) 
+    {
+        $cep = str_replace("-", "", $request->cep);
+
+        $response = Curl::to('viacep.com.br/ws/' . $cep . '/json')->get();
+
+        $response = json_decode($response);
+
+        if ($response) {
+            $data = (object) [
+                'uf' => $response->uf,
+                'zipCode' => $response->zipCode,
+                'city' => $response->localidade
+            ];
+
+            return json_encode($data);
+        }
+    }
+}
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,8 +1,13 @@
 <?php
 
 use App\Http\Controllers\api\VehiclesController;
+use App\Http\Controllers\webservice\WebServiceController;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResources([
     'vehicles' => VehiclesController::class
 ]);
+
+Route::group(['prefix' => 'webservice'], function() {
+    Route::post('cep', [WebServiceController::class, 'cep']);
+});
