ec63db04bad5b78b60a723b498e3a2f91c1f845b
batistondeoliveira <batistondeoliveira@yahoo.com.br>
2021-04-24 20:13:31 -0300

    Alterado o componente Http.js incluindo a constante HttpAuth

--- frontend/src/config/Http.js
+++ frontend/src/config/Http.js
@@ -1,6 +1,29 @@
-import Axios from 'axios'
-import { rootUrl } from './App';
+import Axios from 'axios';
+import { apiUrl, rootUrl } from './App';
 
 export const Http = Axios.create({
     baseURL: rootUrl
-})
+});
+
+export const HttpAuth = Axios.create({
+    baseURL: apiUrl
+});
+
+HttpAuth.interceptors.request.use(
+    async (config) => {
+        config.headers.authorization = 'Bearer ' + await localStorage.getItem('access_tokem');
+
+        return config;
+    }
+);
+
+HttpAuth.interceptors.response.use(response => {
+    return response;
+}, error => {
+    if (error.response) {
+        if (error.response.status === 401) {
+            localStorage.removeItem('access_token');
+            window.location.replace('login');
+        }
+    }
+});
