0e327146f1a02d7290a219de477e8bb4a2697b27
batistondeoliveira <batistondeoliveira@yahoo.com.br>
2021-04-25 18:01:38 -0300

    Alterado a rotina de imagem, incluindo verificacao dos campos inteiros

--- backend/app/Http/Controllers/ImageController.php
+++ backend/app/Http/Controllers/ImageController.php
@@ -10,13 +10,14 @@ use Intervention\Image\ImageManagerStatic as Image;
 class ImageController extends Controller
 {
     public function thumb(Request $request, $path = null, $img = null)
-    {
-        $user = ($request->user) ? (int) $request->user . '/' : '';
+    {                
+        $user = ((int) $request->user) ? (int) $request->user . '/' : '';
         $subPath = ($request->subdir) ? $request->subdir . '/' : '';
-        $width = ($request->width) ? (int) $request->width : null;
-        $height = ($request->height) ? (int) $request->height : null;
+        $width = ((int) $request->width) ? (int) $request->width : null;
+        $height = ((int) $request->height) ? (int) $request->height : null;
         $path = $path . '/' . $subPath . $user . $img;
-        $url = Storage::get($path);
+
+        $url = Storage::get($path);        
 
         if (!$width && !$height) {
             $imagem = Image::cache(function ($image) use($url) {
@@ -25,7 +26,7 @@ class ImageController extends Controller
         } else {
             $imagem = Image::cache(function ($image) use($url, $width, $height) {
                 if ($width && $height) {
-                    $image->>make($url)->fit($width, $height);
+                    $image->make($url)->fit($width, $height);
                 } else {
                     $image->make($url)
                         ->resize($width, $height, function ($constraint) {
