c2990064e224f87c4de34b3a75905c5d952e5962
batistondeoliveira <batistondeoliveira@yahoo.com.br>
2021-04-25 16:18:15 -0300

    Incluido rota para o controller de gravacao, atualizacao e exclusao da imagem

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\api\uploads\VehicleUploadController;
 use App\Http\Controllers\api\VehiclesController;
 use App\Http\Controllers\webservice\WebServiceController;
 use Illuminate\Support\Facades\Route;
@@ -7,6 +8,10 @@ use Illuminate\Support\Facades\Route;
 Route::apiResources([
     'vehicles' => VehiclesController::class
 ]);
+
+Route::prefix('upload')->group(function () {
+    Route::resource('vehicle', VehicleUploadController::class)->only(['create', 'update', 'destroy']);
+});
 
 Route::get('vehicles/{vehicle_type}/brand', [VehiclesController::class, 'brand']);
 Route::get('vehicles/{vehicle_type}/{vehicle_brand}/model', [VehiclesController::class, 'model']);
