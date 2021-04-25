185afd8277ccb216cc4d46b5fba755621f3f2703
batistondeoliveira <batistondeoliveira@yahoo.com.br>
2021-04-25 16:17:15 -0300

    Incluido o controller para baixar a imagem para o front

---
+++ backend/app/Http/Controllers/ImageController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Response;
+use Illuminate\Support\Facades\Storage;
+use Intervention\Image\ImageManagerStatic as Image;
+
+class ImageController extends Controller
+{
+    public function thumb(Request $request, $path = null, $img = null)
+    {
+        $user = ($request->user) ? (int) $request->user . '/' : '';
+        $subPath = ($request->subdir) ? $request->subdir . '/' : '';
+        $width = ($request->width) ? (int) $request->width : null;
+        $height = ($request->height) ? (int) $request->height : null;
+        $path = $path . '/' . $subPath . $user . $img;
+        $url = Storage::get($path);
+
+        if (!$width && !$height) {
+            $imagem = Image::cache(function ($image) use($url) {
+                $image->make($url);
+            });
+        } else {
+            $imagem = Image::cache(function ($image) use($url, $width, $height) {
+                if ($width && $height) {
+                    $image->>make($url)->fit($width, $height);
+                } else {
+                    $image->make($url)
+                        ->resize($width, $height, function ($constraint) {
+                            $constraint->aspectRatio();
+                            $constraint->upsize();
+                        });
+                }
+            });
+        }
+
+        if (isset($imagem)) {
+            return Response::make($imagem, 200, ['Content-Type' => 'image'])
+                    ->setMaxAge(864000) //tempo q a imagem fica armazenada em cache
+                    ->setPublic();
+        }
+    }
+}
