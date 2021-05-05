ab47ab3c7f5c103fe8acc5a813d87c780c654f02
batistondeoliveira <batistondeoliveira@yahoo.com.br>
2021-05-04 22:45:01 -0300

    Atualizado a tabela plans com o campo period

---
+++ backend/database/migrations/2021_05_05_013127_update_plans_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdatePlansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('plans', function (Blueprint $table) {
+            $table->integer('period')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('plans', function (Blueprint $table) {
+            //
+        });
+    }
+}
